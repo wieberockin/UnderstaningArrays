64301ecda4011588e76752e5dcfc2823f1b0f8a5
Ryan Wiebe <wieberockin@yahoo.com>
2018-09-01 20:40:22 -0600

    Reverse an array

--- UnderstaningArrays/Program.cs
+++ UnderstaningArrays/Program.cs
@@ -46,9 +46,34 @@ namespace UnderstaningArrays
 
             string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
 
+            /*
             for (int i = 0; i < names.Length; i++)
             {   
                 Console.WriteLine(names[i])
+            }
+            Console.ReadLine();
+            */
+
+
+            /*
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+                    
+            }
+            Console.ReadLine();
+            */
+
+
+            string zig = "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.WriteLine(zigChar);
             }
             Console.ReadLine();
 
